740578e8ada082074a98782012eb229b42ec8f1d
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-11-13 00:15:14 -0500

    Component Setup

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template>My App</template>
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpense />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpense,
+    TransactionList,
+    AddTransaction,
+  },
+};
+</script>
